e360baef203a3d8bb38b872db2d06920de7c917d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-06-15 23:14:19 -0700

    Attempt at running screenshot creation async'ly thru streams

--- server/screenshot/screenshotController.js
+++ server/screenshot/screenshotController.js
@@ -1,5 +1,6 @@
 const webshot = require('webshot');
 const path = require('path');
+const fs = require('fs');
 
 // const link = 'https://www.mariowiki.com/Mario';
  
@@ -11,15 +12,30 @@ const path = require('path');
 
 // Generates screenshots from links if not made already
 exports.createScreenshots = (req, res, next) => {
-  const portfolioLinks = req.body.portfolio;
+  const portfolio = req.body.portfolio;
 
-  if (!portfolioLinks) return res.status(400).send('No portfolio links provided');
+  if (!portfolio) return res.status(400).send('No portfolio links provided');
 
-  portfolioLinks.forEach((link) => {
+  portfolio.forEach((entry) => {
+    // exclude empty links
+    const link = entry && entry.url;
+    if (!link) return;
+
     // attempt to create photo from link
-    webshot(link, path.resolve('screenshots', link + '.png'), function(err) {
-      err && console.log('Webshot Error:', err);
+    const renderStream = webshot(link);
+    const file = fs.createWriteStream(path.resolve('screenshots', link + '.png'), {
+      encoding: 'binary',
     });
+
+    file.on('open', (fd) => { 
+      // allow read stream to start writing to new file once open
+      renderStream
+        .on('data', data => file.write(data.toString('binary'), 'binary'))
+        .on('end')
+    });
+    file.on('error', err => { throw err });
+
+    renderStream.on('error', err => console.log('Webshot error:', err))
   });
 
   next();
